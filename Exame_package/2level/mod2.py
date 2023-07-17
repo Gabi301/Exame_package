e2d0604fd2ffea6f742a8ab3357dc9ea4a6803c3
Gabi301 <108729223+Gabi301@users.noreply.github.com>
2023-07-17 12:29:38 +0100

    Add files via upload

---
+++ Exame_package/2level/mod2.py
@@ -0,0 +1,15 @@
+def verifica_positivo(a):
+    if a > 0:
+        print(f"O número {a} é positivo")
+    else:
+        print(f"O número {a} é negativo")
+        return a
+
+    
+def verifica_maior_zero(b):
+    if b > 0:
+        print(f"O número {b} é maior que zero")
+    else:
+        print(f"O número {b} é menor que zero")
+        return b
+
